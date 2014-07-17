9cb6395d9f2013a754bef16793a5a15ca49fc030
Zack Fisch <zack@zfisch.com>
2014-07-17 16:33:05 -0400

    Adding page to edit a movie

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Flix::Application.routes.draw do
 	root 'movies#index'
   get 'movies' => 'movies#index'
   get 'movies/:id' => 'movies#show', as: 'movie'
+  get 'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
 end
