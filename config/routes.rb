781ea02084a5cac530639a63468ad6c5f3205847
Zack Fisch <zack@zfisch.com>
2014-07-18 10:46:10 -0400

    Adding ability to create new movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Flix::Application.routes.draw do
 	root 'movies#index'
-  get 'movies' => 'movies#index'
-  get 'movies/:id' => 'movies#show', as: 'movie'
-  get 'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
-  patch 'movies/:id' => 'movies#update'
+  # get 'movies' => 'movies#index'
+  # get 'movies/new' => 'movies#new'
+  # get 'movies/:id' => 'movies#show', as: 'movie'
+  # get 'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
+  # patch 'movies/:id' => 'movies#update'
+  resources :movies
 end
