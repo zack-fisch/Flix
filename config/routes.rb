397cbc4d1e6f8ab882010e1fddd5449cdb20f5d4
Zack Fisch <zack@zfisch.com>
2014-07-18 09:32:36 -0400

    Implenting ability to update/edit movies

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Flix::Application.routes.draw do
   get 'movies' => 'movies#index'
   get 'movies/:id' => 'movies#show', as: 'movie'
   get 'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
+  patch 'movies/:id' => 'movies#update'
 end
