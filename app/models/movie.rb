ce58af153c8df9bc2864a18e7b877e17171bb93c
Zack Fisch <zack@zfisch.com>
2014-07-19 20:03:11 -0400

    Adding custom query to movie model to display already released films ordered by most recent

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ActiveRecord::Base
 	def flop?
 		total_gross.blank? || total_gross < 50000000
 	end
+
+	def self.released
+		where("released_on < ?", Time.now).order("released_on desc")
+	end
 end
