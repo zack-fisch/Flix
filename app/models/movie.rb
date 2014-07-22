3385971a1f89271647c5129aade50c8f858c13a0
Zack Fisch <zack@zfisch.com>
2014-07-22 09:39:12 -0400

    Adding validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,20 @@
 class Movie < ActiveRecord::Base
+
+  validates :title, :released_on, :duration, presence: true
+
+  validates :description, length: { minimum: 25 }
+
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :image_file_name, allow_blank: true, format: {
+  with:    /\w+.(gif|jpg|png)\z/i,
+  message: "must reference a GIF, JPG, or PNG image"
+}
+
+  RATINGS = %w(G PG PG-13 R NC-17)
+
+  validates :rating, inclusion: { in: RATINGS }
+
   def self.released
     where("released_on <= ?", Time.now).order("released_on desc")
   end
