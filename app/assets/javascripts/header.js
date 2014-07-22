43f360a29913998042e7fc42abbed980240e561f
Zack Fisch <zack@zfisch.com>
2014-07-22 17:04:52 -0400

    Adding review model, creating associations between movies and reviews, adding retractable header bar

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom($('#page-header')[0]);
+	// initialise
+	headroom.init();
+});
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
 
+  has_many :reviews
+
   validates :title, :released_on, :duration, presence: true
 
   validates :description, length: { minimum: 25 }
