b04207064a4252c361a0555b15136348e32d48b4
Zack Fisch <zack@zfisch.com>
2014-07-25 11:55:20 -0400

    Adding user new template/action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,12 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Flix::Application.routes.draw do
-
+	get 'signup' => 'users#new'
   resources :users
 
   root "movies#index"
