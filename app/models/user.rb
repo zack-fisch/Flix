5857d2dcb6c4e9149c17840fc829965414684580
Zack Fisch <zack@zfisch.com>
2014-07-24 20:43:45 -0400

    Adding user valdiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :name, presence: true
+
+  validates :email, presence: true, format: /\A\S+@\S+\z/,
+  uniqueness: {case_sensitive: false}
 end
