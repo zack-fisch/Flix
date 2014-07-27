48333a89b481c15b022f36d575c44d59596289a1
Zack Fisch <zack@zfisch.com>
2014-07-27 12:50:21 -0400

    Requiring signing for certain user actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
- 
+  def require_signin
+  	unless current_user
+  		redirect_to new_session_url, alert: 'Please sign in'
+  	end
+  end
 end
