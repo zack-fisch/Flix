51df0f697d86c53b27faf45a3d0aead603850b36
Zack Fisch <zack@zfisch.com>
2014-07-26 20:51:06 -0400

    Refactoring authentication so that logic is contained in controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,10 @@ class User < ActiveRecord::Base
   def gravatar_id
   	Digest::MD5.hexdigest(email.downcase)
   end
+
+  def self.authenticate(email, password)
+  	user = User.find_by(email: email)
+  	user && user.authenticate(password)
+  end
   
 end
