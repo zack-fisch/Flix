457d444bfb4f6b8c01401cfd1b12767d279323af
Zack Fisch <zack@zfisch.com>
2014-07-28 13:47:43 -0400

    Adding authorization for user actions on account (cannot edit/delete another's account)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	before_action :require_signin, except: [:new, :create]
-	
+	before_action :require_correct_user, only: [:edit, :update, :destroy]
+
 	def index
 		@users = User.all
 	end
@@ -24,11 +25,11 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		
 	end
 
 	def update
-		@user = User.find(params[:id])
+		
 		if @user.update(user_params)
 			redirect_to @user
 		else
@@ -37,7 +38,7 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		@user = User.find(params[:id])
+		
 		@user.destroy
 		session[:user_id] = nil
 		redirect_to	root_url, alert: 'Account successfully deleted'
@@ -47,4 +48,12 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
+
+	def require_correct_user
+		@user = User.find(params[:id])
+		unless current_user == @user
+			redirect_to root_url
+		end
+	end
+
 end
