208376e0f45ac1b36d8fc314d2dc8425a919e7fc
Zack Fisch <zack@zfisch.com>
2014-07-28 13:58:30 -0400

    Refactoring display of account modifications options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,5 +14,9 @@ class ApplicationController < ActionController::Base
 		User.find(session[:user_id]) if session[:user_id]
 	end
 
-	helper_method :current_user
+  def current_user?(user)
+    current_user == user
+  end
+
+	helper_method :current_user, :current_user?
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,9 +51,7 @@ class UsersController < ApplicationController
 
 	def require_correct_user
 		@user = User.find(params[:id])
-		unless current_user == @user
-			redirect_to root_url
-		end
+		redirect_to root_url unless current_user?(@user)
 	end
 
 end
