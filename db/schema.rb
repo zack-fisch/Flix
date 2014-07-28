08d1e7e15ebc7839624ae82a58d41996a6e8d4ac
Zack Fisch <zack@zfisch.com>
2014-07-28 14:31:55 -0400

    Adding admin field to Users table to distinguish  between normal/admin users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727011904) do
+ActiveRecord::Schema.define(version: 20140728182951) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140727011904) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
 end
