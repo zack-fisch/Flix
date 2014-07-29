3db54e54971ca327c72a279f6e6794d35ea3c74b
Zack Fisch <zack@zfisch.com>
2014-07-29 18:23:47 -0400

    Establishing model relationships

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
 
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :fans, through: :favorites, source: :user
 
   validates :title, :released_on, :duration, presence: true
 
