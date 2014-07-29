4c54848b24cba0c203bdddb5fa2ae08df6c2b29e
Zack Fisch <zack@zfisch.com>
2014-07-29 16:44:42 -0400

    Editing display of review, must be logged in to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
+	 before_action :require_signin
 	 before_action :set_movie
+
 
 	def index
 		@reviews = @movie.reviews
@@ -11,6 +13,7 @@ class ReviewsController < ApplicationController
 
 	def create
 		 @review = @movie.reviews.new(review_params)
+		 @review.user = current_user
     if @review.save
       redirect_to movie_reviews_path(@movie), notice: 'Thanks for your review!'
     else 
@@ -41,7 +44,7 @@ class ReviewsController < ApplicationController
 
 	private
 	def review_params
-		params.require(:review).permit(:name, :comment, :stars)
+		params.require(:review).permit(:comment, :stars)
 	end
 
 	def set_movie
