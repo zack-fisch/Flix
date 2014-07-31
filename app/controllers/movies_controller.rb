68c4a4feb821816e73f10f81a50a9a55e67a2862
Zack Fisch <zack@zfisch.com>
2014-07-31 16:15:18 -0400

    Adding fans to sidebar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,7 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @fans = @movie.fans
+    @genres = @movie.genres
 
     if current_user
       @current_favorite = current_user.favorites.find_by(movie_id: @movie.id)
@@ -50,7 +51,7 @@ class MoviesController < ApplicationController
 private
 
   def movie_params
-    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name)
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name, :genre_ids)
   end
 end
 
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ActiveRecord::Base
 	validates :name, presence: true, uniqueness: true
+	has_many :characterizations, dependent: :destroy
+	has_many :movies, through: :characterizations
 end
