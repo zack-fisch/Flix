618855847d430db843b713ffb76cadf213e46c04
Zack Fisch <zack@zfisch.com>
2014-07-31 15:06:15 -0400

    Adding unfavorite button to movie page when user has already favorited movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,10 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @fans = @movie.fans
+
+    if current_user
+      @current_favorite = current_user.favorites.find_by(movie_id: @movie.id)
+    end
   end
   
   def edit
