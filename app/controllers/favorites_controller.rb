069e2aff40b9aa7317c9c5edf7d0a75100a018c9
Zack Fisch <zack@zfisch.com>
2014-07-31 15:21:32 -0400

    Removing duplication in favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,22 @@
 class FavoritesController < ApplicationController
 	before_action :require_signin
-	
+	before_action :set_movie
+
 	def create
 		@movie = Movie.find(params[:movie_id])
 		@movie.favorites.create!(user: current_user)
 		redirect_to @movie, notice: 'Thanks for favoriting!'
+	end
+
+	def destroy
+		@movie = Movie.find_by(params[:id])
+		favorite = current_user.favorites.find(params[:id])
+		favorite.destroy
+		redirect_to @movie, notice: 'Movie successfully unfaved'
+	end
+
+	private
+	def set_movie
+		@movie = Movie.find_by(params[:id])
 	end
 end
